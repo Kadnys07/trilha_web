b39db1bf136f00c0f3e880c5caf9eb743106852b
kadnys_martins <kadnysm@gmail.com>
2025-04-08 19:57:26 -0300

    Validando preenchimento do formulário

--- js/site.js
+++ js/site.js
@@ -1,5 +1,30 @@
-alert("Achou!");
-
 function validaFaleConosco() {
-    alerta("Achou!");
+    if ( document.frmfaleconosco.txtnome.value == "") {
+        alert("Preencha o campo Nome");
+        document.frmfaleconosco.txtnome.focus();
+        return false;
+    }
+    
+    if (document.frmfaleconosco.txtfone.value == "") {
+        alert("Preencha o campo Telefone");
+        document.frmfaleconosco.txtfone.focus();
+        return false;
+    }
+    if ( document.frmfaleconosco.txtemail.value == "") {
+        alert("Preencha o campo E-mail");
+        document.frmfaleconosco.email.focus();
+        return false;
+    }
+    if ( document.frmfaleconosco.selmotivo.value == "") {
+        alert("Preencha o campo Motivo");
+        document.frmfaleconosco.selmotivo.focus();
+        return false;
+    }
+    if (document.frmfaleconosco.txtcomentario.value == "") {
+        alert("O campo Comentário deve ter pelo menos 1 caractere e no máximo 500 caracteres");	
+        document.frmfaleconosco.txtcomentario.focus();
+        return false;
+    }
+
+    return true;
 }
