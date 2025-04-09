ae8e44d3f6a9f086a3d0e982da01bc5fa89a4e0b
kadnys_martins <kadnysm@gmail.com>
2025-04-08 21:44:52 -0300

    adicionando fuction verificaMotivo() no site.js

--- js/site.js
+++ js/site.js
@@ -1,30 +1,108 @@
 function validaFaleConosco() {
-    if ( document.frmfaleconosco.txtnome.value == "") {
+    if (document.getElementById("nome").value == "") {
         alert("Preencha o campo Nome");
-        document.frmfaleconosco.txtnome.focus();
+        document.getElementById("nome").focus();
         return false;
     }
-    
-    if (document.frmfaleconosco.txtfone.value == "") {
+
+    if (document.getElementById("fone").value == "") {
         alert("Preencha o campo Telefone");
-        document.frmfaleconosco.txtfone.focus();
+        document.getElementById("fone").focus();
         return false;
     }
-    if ( document.frmfaleconosco.txtemail.value == "") {
+    if (document.getElementById("email").value == "") {
         alert("Preencha o campo E-mail");
-        document.frmfaleconosco.email.focus();
+        document.getElementById("email").focus();
         return false;
     }
-    if ( document.frmfaleconosco.selmotivo.value == "") {
+    if (document.getElementById("motivo").value == "") {
         alert("Preencha o campo Motivo");
-        document.frmfaleconosco.selmotivo.focus();
+        document.getElementById("motivo").focus();
         return false;
     }
-    if (document.frmfaleconosco.txtcomentario.value == "") {
-        alert("O campo Comentário deve ter pelo menos 1 caractere e no máximo 500 caracteres");	
-        document.frmfaleconosco.txtcomentario.focus();
+    if (document.getElementById("comentario").value == "") {
+        alert("O campo Comentário deve ter pelo menos 1 caractere e no máximo 500 caracteres");
+        document.getElementById("comentario").focus();
         return false;
     }
 
     return true;
+}
+function verificaMotivo(motivo) {
+    var elemento = document.getElementById("opcaoProduto");
+
+    if (motivo == "PR") {
+        // cria o elemento select e guardamos na variável homonima
+        var select = document.createElement("select");
+        // Setamos nesse novo select o atributo "name" com o valor "selproduto"
+        select.setAttribute("name", "selproduto");
+
+        //Criamos um elemento (tag) <option> e guardamos na variável homônima
+        var option = document.createElement("option");
+        //Setamos nesse novo option o atributo 'value' com o valor vazio
+        option.setAttribute("value", "");
+        //Criamos um nó de texto "Escolha" e gravamos na variavel 'texto'
+        var texto = document.createTextNode("Escolha");
+        //Colocamos o nó de texto criado como "filho" da tag option criada
+        option.appendChild(texto);
+        //Conteúdo atual da variável option:
+        //<option value="">Escolha</option>
+
+        //Colocamos o option criado como "filho" da tag select criada
+        select.appendChild(option);
+        //Conteúdo atual da variável select:
+        //<select name="selproduto"><option value="">Escolha</option></select>
+
+        //Criamos um elemento (tag) <option> e guardamos na variavel homonima
+        var option = document.createElement("option");
+        //Setamos nesse novo option o atributo 'value' com o valor "FR"
+        option.setAttribute("value", "FR");
+        //Criamos um no de texto "Freezer" e gravamos na variavel 'texto'
+        var texto = document.createTextNode("Freezer");
+        //Colocamos o no de texto criado como "filho" da tag option criada
+        option.appendChild(texto);
+        //Conteudo atual da variável option:
+        //<option value="FR">Freezer</option>
+
+        //Colocamos o option criado como "filho" da tag select criada
+        select.appendChild(option);
+        //Conteudo atual da variavel select:
+        /*
+        <select name="selproduto">
+        <option value="">Escolha</option><option value="FR">Freezer</option>
+        </select>
+        */
+
+        //Criamos um elemento (tag) <option> e guardamos na variavel homonima
+        var option = document.createElement("option");
+        //Setamos nesse novo option o atributo 'value' com o valor "GE"
+        option.setAttribute("value", "GE");
+        //Criamos um no de texto "Geladeira" e gravamos na variavel 'texto"
+        var texto = document.createTextNode("Geladeira");
+        //Colocamos o no de texto criado como "filho" da tag option criada
+        option.appendChild(texto);
+        //Conteúdo atual da variável option:
+        //<option value-"GE">Geladeira</option>
+
+        //Colocamos o option criado como "filho" da tag select criada
+        select.appendChild(option);
+        //Conteudo atual da variavel select:
+
+        // <select name="selproduto">
+        // <option value="">Escolha</option><option value="FR">Freezer</option><option value="GE">Geladeira</option>
+        // </select>
+
+        //Colocamos o select criado como "filho" da tag div capturada no inicio da funcao
+        elemento.appendChild(select);
+
+        //Se o valor da variavel motivo nao for "PR" ...
+    } else {
+        //Se a div possuir algum "primeiro filho"
+        if (elemento.firstChild)
+            //Removemos ele
+            elemento.removeChild(elemento.firstChild);
+
+        1//fim da funcão verificaMotivo
+    }
+
 }
